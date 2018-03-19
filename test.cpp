90c7c7e2b6bec20aae0238d3ebceafe8bd4872c5
Cem Bassoy <cem.bassoy@iosb.fraunhofer.de>
2018-03-19 21:44:43 +0100

    adding timer.h and including it in the test.cpp

--- test.cpp
+++ test.cpp
@@ -3,43 +3,8 @@
 #include <fstream>
 #include <iomanip> 
 
+#include "timer.h"
 #include "matrix.h"
-
-
-namespace iosb
-{
-
-using nanoseconds  = std::chrono::duration<double, std::nano>;
-using microseconds = std::chrono::duration<double, std::micro>;
-using milliseconds = std::chrono::duration<double, std::milli>;
-using seconds      = std::chrono::duration<double, std::ratio<1,1>>;
-template<class D>
-class timer
-{
-public:
-	using clock = std::chrono::high_resolution_clock;
-	using point = typename clock::time_point;
-	
-    static void tic() { _start = clock::now(); }
-    static void toc() { _stop  = clock::now(); }
-
-    static auto elapsed() { return std::chrono::duration_cast<D>( _stop - _start ); }
-
-	template<class O>
-    static auto elapsed() { return std::chrono::duration_cast<O>( _stop - _start ); }
-
-    static point start() { return _start; }
-    static point stop() { return _stop; }
-private:
-    static point _start;
-    static point _stop;
-};
-template<class D> typename timer<D>::point timer<D>::_start;
-template<class D> typename timer<D>::point timer<D>::_stop;
-}
-template<class T>
-std::ostream& operator<< (std::ostream & out, std::chrono::duration<double,T> const& d) 
-{ out << d.count(); return out; }
 
 
 
---
+++ timer.h
@@ -0,0 +1,41 @@
+#ifndef IOSB_TIMER_H
+#define IOSB_TIMER_H
+
+
+namespace iosb
+{
+
+using nanoseconds  = std::chrono::duration<double, std::nano>;
+using microseconds = std::chrono::duration<double, std::micro>;
+using milliseconds = std::chrono::duration<double, std::milli>;
+using seconds      = std::chrono::duration<double, std::ratio<1,1>>;
+template<class D>
+class timer
+{
+public:
+	using clock = std::chrono::high_resolution_clock;
+	using point = typename clock::time_point;
+	
+    static void tic() { _start = clock::now(); }
+    static void toc() { _stop  = clock::now(); }
+
+    static auto elapsed() { return std::chrono::duration_cast<D>( _stop - _start ); }
+
+	template<class O>
+    static auto elapsed() { return std::chrono::duration_cast<O>( _stop - _start ); }
+
+    static point start() { return _start; }
+    static point stop() { return _stop; }
+private:
+    static point _start;
+    static point _stop;
+};
+template<class D> typename timer<D>::point timer<D>::_start;
+template<class D> typename timer<D>::point timer<D>::_stop;
+}
+template<class T>
+std::ostream& operator<< (std::ostream & out, std::chrono::duration<double,T> const& d) 
+{ out << d.count(); return out; }
+
+
+#endif
